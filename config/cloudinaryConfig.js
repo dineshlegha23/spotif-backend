f17f2a09213673076c97979b545d10fbaa7348c1
Dinesh <dineshlegha23@gmail.com>
2024-08-27 17:31:34 +0530

    added cloudinary config

---
+++ config/cloudinaryConfig.js
@@ -0,0 +1,11 @@
+const { v2: cloudinary } = require("cloudinary");
+
+const uploadOnCloudinary = async function () {
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET, // Click 'View API Keys' above to copy your API secret
+  });
+};
+
+module.exports = uploadOnCloudinary;
